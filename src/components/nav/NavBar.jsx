f77144c2b6e528fd10570098502d4eb707417293
Tricia Swift <tricia.swift8@gmail.com>
2023-12-06 11:39:01 -0600

    Added navbard and updated authorization

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,3 +1,41 @@
-export const NavBar = () => {
-  return <></>;
+/* eslint-disable react/prop-types */
+import { Link, useNavigate } from "react-router-dom";
+
+export const NavBar = ({ token, setToken }) => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar p-0" style={{ backgroundColor: "#e3f2fd" }}>
+      <div className="container-fluid">
+        <a className="navbar-brand">Tastevine</a>
+        <div className="flex max-h-full">
+          {token ? <div>Navbar for logged in user</div> : ""}
+        </div>
+        <div>
+          {token ? (
+            <div className="hover:bg-cyan-600/90 py-2.5 px-3">
+              <button
+                className=""
+                onClick={() => {
+                  setToken("");
+                  navigate("/login");
+                }}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className="flex">
+              <div className="mr-2 py-2.5 px-3 hover:bg-cyan-600/90">
+                <Link to="/register">Register</Link>
+              </div>
+              <div className="mr-2 py-2.5 px-3 hover:bg-cyan-600/90">
+                <Link to="/login">Login</Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
